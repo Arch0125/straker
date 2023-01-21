92b2536502d9e664fcfd7e949939046a9c5e1b62
Archisman Das <dasarchisman25@gmail.com>
2023-01-22 00:23:36 +0530

    Add daix faucet and change sidemenu

--- components/Sidemenu.tsx
+++ components/Sidemenu.tsx
@@ -8,14 +8,22 @@ const Sidemenu: React.FunctionComponent<ISidemenuProps> = (props) => {
 
   const[streampage, setStreampage] = React.useState(false);
   const[stakepage, setStakepage] = React.useState(true);
+  const[faucetpage, setFaucetpage] = React.useState(false);
 
   const switchPage = (page: string) => {
     if(page === 'stream'){
       setStreampage(true);
       setStakepage(false);
+      setFaucetpage(false);
     } else if(page === 'stake'){
       setStreampage(false);
       setStakepage(true);
+      setFaucetpage(false);
+    }
+    else if(page === 'faucet'){
+      setStreampage(false);
+      setStakepage(false);
+      setFaucetpage(true);
     }
   };
 
@@ -28,7 +36,8 @@ const Sidemenu: React.FunctionComponent<ISidemenuProps> = (props) => {
     <div>
         <ul className="menu bg-base border-2 border-primary text-secondary w-56 p-2 rounded-box  ">
             <li className='text-primary' onClick={(e)=>switchPage("stream")} ><a className={` ${streampage === true ? "text-secondary active":"text-primary"} `}>Wrap/Unwrap</a></li>
-            <li onClick={(e)=>switchPage("stake")}><a className={` ${stakepage === true ? "text-secondary active":"text-primary"} `}><Link href={'/Stakelist'} >Stake</Link></a></li>
+            <Link href={'/Stakelist'} ><li onClick={(e)=>switchPage("stake")}><a className={` ${stakepage === true ? "text-secondary active":"text-primary"} `}>Stake</a></li></Link>
+            <Link href={'/Faucet'} ><li onClick={(e)=>switchPage("faucet")}><a className={` ${faucetpage === true ? "text-secondary active":"text-primary"} `}>Faucet</a></li></Link>
         </ul>
     </div>
   );
